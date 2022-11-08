8f9790150f7e68b5a0f1b10ce52853c95f853ce9
Muammer Diri <muammerdiri2@gmail.com>
2022-11-08 12:25:19 +0300

    import fix

--- src/main/java/Kodlama/Io/Devs/business/abstracts/ProgrammingLanguageService.java
+++ src/main/java/Kodlama/Io/Devs/business/abstracts/ProgrammingLanguageService.java
@@ -2,14 +2,15 @@ package Kodlama.Io.Devs.business.abstracts;
 
 import java.util.List;
 
-import Kodlama.Io.Devs.business.requests.CreateProgrammingLanguageRequest;
-import Kodlama.Io.Devs.business.requests.UpdateProgrammingLanguageRequest;
-import Kodlama.Io.Devs.business.responses.GetAllProgrammingLanguageResponse;
+import Kodlama.Io.Devs.business.requests.programmingLanguage.CreateProgrammingLanguageRequest;
+import Kodlama.Io.Devs.business.requests.programmingLanguage.UpdateProgrammingLanguageRequest;
+import Kodlama.Io.Devs.business.responses.programmingLanguage.GetAllProgrammingLanguageResponse;
 
 public interface ProgrammingLanguageService {
 	List<GetAllProgrammingLanguageResponse> getAll();
 	void add(CreateProgrammingLanguageRequest createRequest);
 	void update(int id, UpdateProgrammingLanguageRequest updateRequest);
 	void delete(int id);
+	
 
 }
