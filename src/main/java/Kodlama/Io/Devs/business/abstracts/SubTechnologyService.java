133a4dc2a4e03f6e9a8b319db0827e0aaba04983
Muammer Diri <muammerdiri2@gmail.com>
2022-11-08 12:28:06 +0300

    Added update and delete functions

--- src/main/java/Kodlama/Io/Devs/business/abstracts/SubTechnologyService.java
+++ src/main/java/Kodlama/Io/Devs/business/abstracts/SubTechnologyService.java
@@ -2,10 +2,13 @@ package Kodlama.Io.Devs.business.abstracts;
 
 import java.util.List;
 
-import Kodlama.Io.Devs.business.requests.CreateSubTechnologyRequest;
-import Kodlama.Io.Devs.business.responses.GetAllSubTechnologyResponse;
+import Kodlama.Io.Devs.business.requests.subTechnology.CreateSubTechnologyRequest;
+import Kodlama.Io.Devs.business.requests.subTechnology.UpdateSubTechnologyRequest;
+import Kodlama.Io.Devs.business.responses.subTechnology.GetAllSubTechnologyResponse;
 
 public interface SubTechnologyService {
 	List<GetAllSubTechnologyResponse> getAll();
 	void add(CreateSubTechnologyRequest createRequest);
+	void update(int id,UpdateSubTechnologyRequest updateRequest);
+	void delete(int id);
 }
